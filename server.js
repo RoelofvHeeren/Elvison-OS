a556dc0b0c8d8ebdbbb3794d6c97479cdc9e0e26
roelofvanheeren <roelof@elvison.com>
2026-01-11 17:19:10 +0800

    Add global request logging

--- server.js
+++ server.js
@@ -27,6 +27,12 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 
+// DEBUG: Log all requests
+app.use((req, res, next) => {
+    console.log(`[REQUEST] ${req.method} ${req.url}`);
+    next();
+});
+
 // --- API Endpoints (Static files served at the end) ---
 
 // Health Check
